1b9ec474871cf3f151f276cf492bc28bb0675ae5
Anthony COLAS <acolas@e1r10p15.42.fr>
2017-05-30 15:10:01 +0200

    ft_check posi norm

--- ft_check_posi.c
+++ ft_check_posi.c
@@ -6,23 +6,21 @@
 /*   By: llorgere <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/30 13:04:51 by llorgere          #+#    #+#             */
-/*   Updated: 2017/05/30 14:39:08 by llorgere         ###   ########.fr       */
+/*   Updated: 2017/05/30 15:09:19 by acolas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 int		ft_check_posi(char **square, int **tetra, int row, int col, int n)
 {
 	int		i;
-//	char	**squareret;
 
 	i = 0;
 	while (i < 4)
 	{
 		if ((row + tetra[i][0]) < 0 || (col + tetra[i][1]) < 0
-			|| (row + tetra[i][0]) >= n || (col + tetra[i][1]) >= n)
+				|| (row + tetra[i][0]) >= n || (col + tetra[i][1]) >= n)
 			return (0);
-		else if(square[row + tetra[i][0]][col + tetra[i][1]] != '.')
+		else if (square[row + tetra[i][0]][col + tetra[i][1]] != '.')
 			return (0);
 		i++;
 	}
