89d3ca8a847c18049317d854c5d7e4755dae18c3
Anthony COLAS <acolas@e1r10p15.42.fr>
2017-05-30 16:04:57 +0200

    ft_check_tetra norm

--- ft_check_tetra.c
+++ ft_check_tetra.c
@@ -6,50 +6,49 @@
 /*   By: llorgere <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/15 16:13:51 by llorgere          #+#    #+#             */
-/*   Updated: 2017/05/15 18:11:44 by llorgere         ###   ########.fr       */
+/*   Updated: 2017/05/30 16:01:22 by acolas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft/libft.h"
-#include <stdio.h>
+
+#include "libfil.h"
+
+int		ft_tetra_check(char *tetra, int i, int l)
+{
+	int		la;
+
+	la = 0;
+	if (tetra[i - 1] == '#')
+		la++;
+	if (tetra[i + 1] == '#')
+		la++;
+	if (tetra[i - 5] == '#')
+		la++;
+	if (tetra[i + 5] == '#')
+		la++;
+	if (la == 0)
+		return (0);
+	l = l + la;
+	return (l);
+}
 
 int		ft_check_tetra(char *tetra)
 {
 	int		i;
 	int		l;
 	int		j;
-	int		la;
 
 	i = 0;
 	j = i + 4;
 	l = 0;
 	while (i <= 19)
 	{
-//		printf("test 1 du ft_check");
-		while (i < j)
+		while (i < j && (tetra[i] == '.' || tetra[i] == '#'))
 		{
-//			printf("test 2 du ft_check");
-			if (tetra[i] == '.')
-				i++;
-			else if (tetra[i] == '#')
-			{
-				la = 0;
-				if (tetra[i - 1] == '#')
-					la++;
-				if (tetra[i + 1] == '#')
-					la++;
-				if (tetra[i - 5] == '#')
-					la++;
-				if (tetra[i + 5] == '#')
-					la++;
-				if (la == 0)
-					return (0);
-				l = l + la;
-				i++;
-			}
-			else
-				return (0);
+			if (tetra[i] == '#')
+				l = ft_tetra_check(tetra, i, l);
+			i++;
 		}
-		if (tetra[i] != '\n')
+		if (tetra[i] != '\n' && i != j)
 			return (0);
 		i++;
 		j = i + 4;
