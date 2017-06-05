e910fd800847b045ea150acb56b78e842becd1f1
Anthony COLAS <neo_a2@anthonys-macbook.home>
2017-06-05 22:51:26 +0200

    memchr

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -12,18 +12,17 @@
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *str, int c, size_t n)
 {
-	size_t			i;
-	unsigned char	*tmp;
+	int		i;
 
 	i = 0;
-	tmp = (unsigned char *)s;
-	while (i < n)
+	while (n > 0)
 	{
-		if (tmp[i] == (unsigned char)c)
-			return (void *)(&(s[i]));
+		if (*((unsigned char *)str + i) == (unsigned char)c)
+			return ((void *)str + i);
+		n--;
 		i++;
 	}
-	return (NULL);
+	return (0);
 }
